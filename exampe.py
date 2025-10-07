393bd29851e9886c21cc7b2c2c7e0d20ee230ac5
tdeshaimanojkumarreddy-source <tdeshaimanojkumarreddy@gmail.com>
2025-10-07 18:42:44 +0530

    10 line added

---
+++ exampe.py
@@ -0,0 +1,10 @@
+name = 'Bob' # str
+age = 12 #int
+studying = True #bool
+height = 12.3 #float
+test = '12.3'
+
+print('Name =',name,'\n',"Age=",age,'\n',studying,height,test)
+
+
+print("hello world")
